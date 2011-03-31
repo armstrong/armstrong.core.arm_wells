cf9ba0d497127d5261c8bee5eebe0e8f12aabf3a
Travis Swicegood <development@domain51.com>
2011-03-31 11:23:19 -0500

    Make these exception messages translatable

--- armstrong/core/arm_wells/views.py
+++ armstrong/core/arm_wells/views.py
@@ -1,21 +1,22 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.views.generic import TemplateView
+from django.utils.translation import ugettext as _
 
 
 class SimpleWellView(TemplateView):
     def render_to_response(self, context, **response_kwargs):
         if not 'params' in context:
             raise ImproperlyConfigured(
-                    "Expects `params` to be provided in the context")
+                    _(u"Expects `params` to be provided in the context"))
         if not self.template_name:
             if not 'template_name' in context['params']:
                 raise ImproperlyConfigured(
-                        "Expects `template_name` to be provided")
+                        _(u"Expects `template_name` to be provided"))
             else:
                 self.template_name = context['params']['template_name']
 
         if not 'well' in context['params']:
-            raise ImproperlyConfigured("Expects a `well` to be provided")
+            raise ImproperlyConfigured(_(u"Expects a `well` to be provided"))
 
         return super(SimpleWellView, self).render_to_response(context,
                 **response_kwargs)
