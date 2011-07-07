26eea7450ed2f293d3c57edf651252ba53504d7c
Dave McLain <dmclain@texastribune.org>
2011-07-07 11:27:36 -0500

    use the new setup.py/package.json

--- setup.py
+++ setup.py
@@ -1,9 +1,38 @@
+"""
+setup.py file for building armstrong components.
+
+Nothing in this file should need to be edited, please see accompanying
+package.json file if you need to adjust metadata about this package.
+"""
+
 from distutils.core import setup
+import json
 import os
 
-PACKAGE_NAME = "armstrong.core.arm_wells"
-VERSION = ("0", "1", "0", "2")
+info = json.load(open("./package.json"))
+
+
+def convert_to_str(d):
+    """
+    Recursively convert all values in a dictionary to strings
+
+    This is required because setup() does not like unicode in
+    the values it is supplied.
+    """
+    d2 = {}
+    for k, v in d.items():
+        k = str(k)
+        if type(v) in [list, tuple]:
+            d2[k] = [str(a) for a in v]
+        elif type(v) is dict:
+            d2[k] = convert_to_str(v)
+        else:
+            d2[k] = str(v)
+    return d2
+
+info = convert_to_str(info)
 NAMESPACE_PACKAGES = []
+
 
 # TODO: simplify this process
 def generate_namespaces(package):
@@ -11,7 +40,8 @@ def generate_namespaces(package):
     if new_package.count(".") > 0:
         generate_namespaces(new_package)
     NAMESPACE_PACKAGES.append(new_package)
-generate_namespaces(PACKAGE_NAME)
+generate_namespaces(info["name"])
+
 
 if os.path.exists("MANIFEST"):
     os.unlink("MANIFEST")
@@ -37,7 +67,7 @@ def build_package(dirpath, dirnames, filenames):
         packages.append(pkg)
     elif filenames:
         # Strip off the length of the package name plus the trailing slash
-        prefix = dirpath[len(PACKAGE_NAME) + 1:]
+        prefix = dirpath[len(info["name"]) + 1:]
         for f in filenames:
             # Ignore all dot files and any compiled
             if f.startswith(".") or f.endswith(".pyc"):
@@ -46,19 +76,16 @@ def build_package(dirpath, dirnames, filenames):
 
 
 [build_package(dirpath, dirnames, filenames) for dirpath, dirnames, filenames
-        in os.walk(PACKAGE_NAME.replace(".", "/"))]
+        in os.walk(info["name"].replace(".", "/"))]
 
-setup(
-    name=PACKAGE_NAME,
-    version=".".join(VERSION),
-    description='Provides the basic content well manipulation',
-    author='Bay Citizen & Texas Tribune',
-    author_email='info@armstrongcms.org',
-    url='http://github.com/armstrong/armstrong.core.arm_wells/',
-    packages=packages,
-    package_data={PACKAGE_NAME: data_files, },
-    namespace_packages=NAMESPACE_PACKAGES,
-    classifiers=[
+setup_kwargs = {
+    "author": "Bay Citizen & Texas Tribune",
+    "author_email": "dev@armstrongcms.org",
+    "url": "http://github.com/armstrong/%s/" % info["name"],
+    "packages": packages,
+    "package_data": {info["name"]: data_files, },
+    "namespace_packages": NAMESPACE_PACKAGES,
+    "classifiers": [
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -67,4 +94,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
-)
+}
+
+setup_kwargs.update(info)
+setup(**setup_kwargs)
