0dbbf3299403fcf80c5f6741f07d4d8579b53134
Dave McLain <dmclain@texastribune.org>
2011-10-25 15:20:55 -0500

    Make get_current explicity pull the most recent (v1.1.1)

--- armstrong/core/arm_wells/managers.py
+++ armstrong/core/arm_wells/managers.py
@@ -6,7 +6,8 @@ class WellManager(models.Manager):
     def get_current(self, title):
         now = datetime.datetime.now()
         queryset = self.filter(type__title=title, pub_date__lte=now,
-                active=True).exclude(expires__lte=now, expires__isnull=False)
+                active=True).exclude(expires__lte=now,
+                        expires__isnull=False).order_by('-pub_date')
 
         try:
             return queryset[0]
