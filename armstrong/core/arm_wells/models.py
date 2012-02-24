36b29060d20078905a81625d61f2380dcf356cb4
Jon Cotton <jcotton@baycitizen.org>
2012-02-24 12:55:19 -0800

    Wells as Mixins

--- armstrong/core/arm_wells/models.py
+++ armstrong/core/arm_wells/models.py
@@ -1,69 +1,15 @@
-import datetime
 from django.db import models
-from django.db.models.query import QuerySet
-from django.contrib.contenttypes import generic
-from django.contrib.contenttypes.models import ContentType
 
-from .managers import WellManager
-from .querysets import MergeQuerySet, GenericForeignKeyQuerySet
+from .mixins import WellTypeMixin, WellMixin, WellNodeMixin
 
 
-class WellType(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField()
-
-    def __unicode__(self):
-        return self.title
+class WellType(WellTypeMixin):
+    pass
 
 
-class Well(models.Model):
+class Well(WellMixin):
     type = models.ForeignKey(WellType)
-    pub_date = models.DateTimeField()
-    expires = models.DateTimeField(null=True, blank=True)
-    active = models.BooleanField(default=True)
-
-    objects = WellManager()
-
-    def __init__(self, *args, **kwargs):
-        super(Well, self).__init__(*args, **kwargs)
-        self.queryset = None
-
-    @property
-    def title(self):
-        return self.type.title
-
-    def __unicode__(self):
-        return "%s (%s - %s)" % (self.type, self.pub_date,
-                                 self.expires or "Never")
-
-    def save(self, *args, **kwargs):
-        if not self.pub_date:
-            self.pub_date = datetime.datetime.now()
-        return super(Well, self).save(*args, **kwargs)
-
-    @property
-    def items(self):
-        node_qs = GenericForeignKeyQuerySet(self.nodes.all().select_related())
-        if self.queryset is None:
-            return node_qs
-        else:
-            return MergeQuerySet(node_qs, self.queryset)
-
-    def merge_with(self, queryset):
-        self.queryset = queryset
-        return self.items
 
 
-class Node(models.Model):
+class Node(WellNodeMixin):
     well = models.ForeignKey(Well, related_name="nodes")
-    order = models.IntegerField(default=0)
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
-
-    class Meta:
-        ordering = ["order"]
-
-    def __unicode__(self):
-        return u"%s (%d): %s" % (self.well.title, self.order,
-                                self.content_object)
