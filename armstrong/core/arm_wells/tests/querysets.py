bfeb979a22235d17358984efff568c9b5e1df155
Jon Cotton <jcotton@baycitizen.org>
2012-02-27 16:05:58 -0800

    Add a test for all() on GFKQuerySet and MergeQuerySet.

--- armstrong/core/arm_wells/tests/querysets.py
+++ armstrong/core/arm_wells/tests/querysets.py
@@ -24,10 +24,12 @@ class GenericForeignKeyQuerySetTestCase(TestCase):
         for i in range(self.number_of_extra_stories):
             self.extra_stories.append(generate_random_story())
 
-    def test_raises_NotImplementedError_on_exclude(self):
+    def test_raises_NotImplementedError_on_all_and_exclude(self):
+        gfk_qs = GenericForeignKeyQuerySet(self.well.nodes.all()\
+                .select_related())
         with self.assertRaises(NotImplementedError):
-            gfk_qs = GenericForeignKeyQuerySet(self.well.nodes.all()\
-                    .select_related())
+            gfk_qs.all()
+        with self.assertRaises(NotImplementedError):
             gfk_qs.exclude()
 
     def test_raises_NotImplementedError_on_misc_functions(self):
@@ -49,7 +51,6 @@ class GenericForeignKeyQuerySetTestCase(TestCase):
                 .select_related())
         with self.assertRaises(AttributeError):
             getattr(gfk_qs, "unknown_and_unknowable")
-
 
     def test_gathers_all_nodes_of_one_type_with_two_queries(self):
         gfk_qs = GenericForeignKeyQuerySet(self.well.nodes.all()\
@@ -178,9 +179,11 @@ class MergeQuerySetTestCase(TestCase):
             generate_random_image()
         self.qs_b = Image.objects.all()
 
-    def test_raises_NotImplementedError_on_exclude(self):
+    def test_raises_NotImplementedError_on_all_and_exclude(self):
+        merge_qs = MergeQuerySet(self.qs_a, self.qs_b)
         with self.assertRaises(NotImplementedError):
-            merge_qs = MergeQuerySet(self.qs_a, self.qs_b)
+            merge_qs.all()
+        with self.assertRaises(NotImplementedError):
             merge_qs.exclude()
 
     def test_raises_NotImplementedError_on_misc_functions(self):
