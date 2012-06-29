5463467df05b44ecde622a6de36dc79a824b450b
Jon Cotton <jcotton@baycitizen.org>
2012-06-28 20:50:15 -0700

    WellManager's `get_current()` can now find Wells using `title` or `slug`.
    In answer to armstrong/armstrong#74
    The method takes kwargs for title or slug, but defaults to `title` if using an arg to maintain backward compatibility.

--- armstrong/core/arm_wells/managers.py
+++ armstrong/core/arm_wells/managers.py
@@ -3,8 +3,17 @@ from django.db import models
 
 
 class WellManager(models.Manager):
-    def get_current(self, title):
+    def get_current(self, title=None, slug=None):
         now = datetime.datetime.now()
-        return self.filter(type__title=title, pub_date__lte=now,
-                active=True).exclude(expires__lte=now,
-                        expires__isnull=False).latest('pub_date')
+        filter_params = dict(pub_date__lte=now, active=True)
+
+        if (title):
+            filter_params['type__title'] = title
+        elif (slug):
+            filter_params['type__slug'] = slug
+        else:
+            raise self.model.DoesNotExist()
+
+        return (self.filter(**filter_params)
+                    .exclude(expires__lte=now, expires__isnull=False)
+                    .latest('pub_date'))
