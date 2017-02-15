ddf6cfae0ae5593735f4eaa9a1e5cfa4fa6b4423
Liam Andrew <landrew@texastribune.org>
2017-02-15 13:06:21 -0600

    Delete tests everywhere bc they cause headaches

--- armstrong/core/arm_wells/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-from .managers import *
-from .models import *
-from .query import *
-from .querysets import *
-from .views import *
--- fabfile.py
+++ fabfile.py
@@ -5,16 +5,14 @@ settings = {
     'INSTALLED_APPS': (
         'django.contrib.contenttypes',
         'armstrong.core.arm_wells',
-        'armstrong.core.arm_wells.tests.arm_wells_support',
         'south',
     ),
     'TEMPLATE_CONTEXT_PROCESSORS': (
         'django.core.context_processors.request',
     ),
-    'ROOT_URLCONF': 'armstrong.core.arm_wells.tests.arm_wells_support.urls',
 }
 
 main_app = "arm_wells"
 full_name = "armstrong.core.arm_wells"
-tested_apps = (main_app, 'arm_wells_support')
+tested_apps = (main_app,)
 pip_install_first = True
